803d40e0e111e6e0752a392e4d7d7f80c93cada5
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-11-24 18:41:39 +0900

    add Q01

---
+++ q01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Q01
+"""
+
+N = 11
+while True:
+    if str(N) == str(N)[::-1] and \
+    "{0:o}".format(N) == "{0:o}".format(N)[::-1] and \
+    "{0:b}".format(N) == "{0:b}".format(N)[::-1]:
+        print(N)
+        break
+    N += 2
