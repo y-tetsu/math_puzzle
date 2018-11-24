56229e5e20353325e6581db47c4348bdccb63eb2
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-11-24 20:38:23 +0900

    modify regexp

--- q02.py
+++ q02.py
@@ -16,7 +16,7 @@ for val in range(1000, 10000):
                 form = c[3] + op1 + c[2] + op2 + c[1] + op3 + c[0]
 
                 # 数値の先頭の0は消す
-                form = re.sub(r'0+(\d)', r'\1', form)
+                form = re.sub(r'(^|\W+)0+(\d)', r'\1\2', form)
 
                 # 0割りを除外する
                 if not re.search(r'/0', form):
