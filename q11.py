38572cacb60e5fdba5ff24c67ec3d9a8da51db8b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-01 17:55:52 +0900

    add Q11

---
+++ q11.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Q11
+"""
+A = B = 1
+COUNT = 0
+
+while COUNT < 11:
+    C = A + B
+
+    if C % sum([int(i) for i in str(C)]) == 0:
+        if COUNT > 5:
+            print(C)
+
+        COUNT += 1
+
+    A, B = B, C
