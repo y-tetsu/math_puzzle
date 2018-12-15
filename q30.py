2ccaac65c4472966e049f09a02718f2c82989269
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-16 08:17:19 +0900

    add Q30

---
+++ q30.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Q30
+"""
+
+N = 20
+MEMO = {"1": 1}
+
+
+def set_tap(remain):
+    """
+    テーブルタップをつなぐ
+    """
+    if str(remain) in MEMO:
+        return MEMO[str(remain)]
+
+    cnt = 0
+    for i in range(1, int(remain / 2) + 1):
+        if remain - i == i:
+            cnt += set_tap(i) * (set_tap(i) + 1) / 2
+        else:
+            cnt += set_tap(remain - i) * set_tap(i)
+
+    for i in range(1, int(remain / 3) + 1):
+        for j in range(i, int((remain - i) / 2) + 1):
+            if remain - (i + j) == i and i == j:
+                cnt += set_tap(i) * (set_tap(i) + 1) * (set_tap(i) + 2) / 6
+            elif remain - (i - j) == i:
+                cnt += set_tap(i) * (set_tap(i) + 1) * set_tap(j) / 2
+            elif i == j:
+                cnt += set_tap(remain - (i + j)) * set_tap(i) * (set_tap(i) + 1) / 2
+            elif remain - (i + j) == j:
+                cnt += set_tap(j) * (set_tap(j) + 1) * set_tap(i) / 2
+            else:
+                cnt += set_tap(remain - (i + j)) * set_tap(j) * set_tap(i)
+
+    MEMO[str(remain)] = int(cnt)
+
+    return int(cnt)
+
+
+print(set_tap(N))
