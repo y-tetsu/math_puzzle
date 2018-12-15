2b2344c55c2b3b168a23ebfd4658419b725c7b33
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-15 11:29:05 +0900

    add Q23

---
+++ q23.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Q23
+"""
+
+C = 10
+G = 24
+
+
+def blackjack(coin, game):
+    """
+    ブラックジャック
+    """
+    if coin <= 0:
+        return 0
+
+    if coin >= 1 and game >= G:
+        return 1
+
+    cnt = 0
+    cnt += blackjack(coin + 1, game + 1)
+    cnt += blackjack(coin - 1, game + 1)
+
+    return cnt
+
+
+if __name__ == "__main__":
+    print("cnt =", blackjack(C, 0))
