878e39462f8ec2464bf260acdb183daaf393d233
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-16 10:54:49 +0900

    add Q39

---
+++ q39.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Q39
+"""
+
+MEMO = {0: 0}
+
+# 反転用マスク
+MASK = [0 for i in range(16)]
+for row in range(4):
+    for col in range(4):
+        MASK[row * 4 + col] = (0b1111 << (row * 4)) | (0b1000100010001 << col)
+
+
+def search(ptn, depth):
+    """
+    探索
+    """
+    # すべての初期状態が確認済み
+    if len(MEMO) == 65536:
+        return depth
+
+    # 次のパターン
+    next_ptn = []
+    next_depth = depth + 1
+
+    for i in ptn:
+        for j in range(16):
+            tmp = i ^ MASK[j]
+
+            if tmp not in MEMO:
+                MEMO[tmp] = next_depth
+                next_ptn.append(tmp)
+
+    return search(next_ptn, next_depth)
+
+
+CNT = search([0], 0)
+print(CNT, ["{0:016b}".format(k) for k, v in MEMO.items() if v == CNT])
