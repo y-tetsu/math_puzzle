627820985bc58f8ea1fc16e3bba004962edff888
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-29 10:57:09 +0900

    add Q46

---
+++ q46.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Q46
+"""
+
+import itertools
+
+N = 7
+C = 0
+
+for i in itertools.permutations([i + 1 for i in range(N)]):
+    ptn = list(i)
+    cnt = 0
+
+    for j in range(len(ptn)):
+        k = ptn.index(j + 1)
+
+        if j != k:
+            ptn[j], ptn[k] = ptn[k], ptn[j]
+            cnt += 1
+
+    C += cnt
+
+print("result =", C)
