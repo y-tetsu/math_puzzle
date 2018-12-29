a36b8686f6e61bbeb71ca94b7eb1dadee731979b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-29 11:05:47 +0900

    add Q47

---
+++ q47.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Q47
+"""
+
+import itertools
+
+N = 4
+MEMO = {}
+
+for pattern in itertools.product([1, 0], repeat=N*N):
+    count = []
+
+    for col in range(N):
+        count_row, count_col = 0, 0
+
+        for row in range(N):
+            count_row += pattern[col+row*N]
+            count_col += pattern[col*N+row]
+
+        count += [count_row, count_col]
+
+    MEMO[tuple(count)] = 1 if tuple(count) not in MEMO else MEMO[tuple(count)] + 1
+
+print(sum(filter(lambda x: x == 1, map(lambda x: MEMO[x], MEMO))))
