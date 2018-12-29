286c85dd56356f6a344d58b988306bd6c22d5fb5
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-29 11:32:15 +0900

    add Q48

---
+++ q48.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Q48
+"""
+
+
+def gray_loop(value, base):
+    """
+    グレイコードのループ
+    """
+    g_next = value
+    cnt = 1
+
+    while True:
+        g_shift = "0" + g_next[0:-1]
+        g_exor = ""
+
+        for i, j in zip(g_next, g_shift):
+            g_exor += str("{0:x}".format((int(i, base) - int(j, base)) % base))
+
+        if g_exor == value:
+            break
+        else:
+            cnt += 1
+            g_next = g_exor
+
+    return cnt
+
+
+print("result of 808080 =", gray_loop("808080", 16))
+print("result of abcdef =", gray_loop("abcdef", 16))
