415ea465b4ac158d315e269191043bab7db68d43
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2018-12-31 17:09:31 +0900

    add Q54

---
+++ q54.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Q54
+"""
+
+N = 11
+
+
+def search(cards, num):
+    """
+    探索
+    """
+    if num == 0:
+        return 1
+
+    cnt = 0
+
+    for i, j in enumerate(cards):
+        if j == 0 and i + num + 1 < len(cards) and cards[i + num + 1] == 0:
+            cards[i] = cards[i + num + 1] = num
+            cnt += search(cards, num - 1)
+            cards[i] = cards[i + num + 1] = 0
+
+    return cnt
+
+
+print(search([0 for i in range(N * 2)], N))
