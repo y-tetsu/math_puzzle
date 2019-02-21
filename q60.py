91608d322df18430ede6298fc88ce981aee678cb
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-02-21 15:57:03 +0900

    add Q60

---
+++ q60.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Q60
+"""
+
+import itertools
+
+N = 4
+CNT1 = CNT2 = PTN = 0
+
+
+def check1(width, length):
+    """
+    結合できるパターン
+    """
+    for index1 in range(N - 1):
+        for index2 in range(N - 1):
+            width1 = width[index1 * (N - 1) + index2]
+            width2 = width[(index1 + 1) * (N - 1) + index2]
+            length1 = length[index1 * N + index2]
+            length2 = length[index1 * N + index2 + 1]
+
+            # 十字格子のうち2箇所結合するNGパターン
+            ng2_1 = width1 and not width2 and length1 and not length2
+            ng2_2 = not width1 and width2 and length1 and not length2
+            ng2_3 = width1 and not width2 and not length1 and length2
+            ng2_4 = not width1 and width2 and not length1 and length2
+            cond1 = ng2_1 or ng2_2 or ng2_3 or ng2_4
+
+            # 十字格子のうち3箇所結合するNGパターン
+            ng3_1 = width1 and width2 and length1 and not length2
+            ng3_2 = width1 and width2 and not length1 and length2
+            ng3_3 = width1 and not width2 and length1 and length2
+            ng3_4 = not width1 and width2 and length1 and length2
+            cond2 = ng3_1 or ng3_2 or ng3_3 or ng3_4
+
+            if cond1 or cond2:
+                return False
+
+    return True
+
+
+def check2(width, length):
+    """
+    1×1のセルがないように結合できるパターン
+    """
+    for index1 in range(N):
+        for index2 in range(N):
+            width1 = width2 = length1 = length2 = 0
+
+            if index2 > 0:
+                width1 = width[index1 * (N - 1) + index2 - 1]
+
+            if index2 < N - 1:
+                width2 = width[index1 * (N - 1) + index2]
+
+            if index1 > 0:
+                length1 = length[(index1 - 1) * N + index2]
+
+            if index1 < N - 1:
+                length2 = length[index1 * N + index2]
+
+            if not width1 and not width2 and not length1 and not length2:
+                return False
+
+    return True
+
+
+for i in itertools.product([0, 1], repeat=N * (N - 1)):
+    for j in itertools.product([0, 1], repeat=N * (N - 1)):
+        PTN += 1
+
+        if (PTN + 1) % 1000000 == 1:
+            print(PTN)
+
+        if check1(i, j):
+            CNT1 += 1
+
+            if check2(i, j):
+                CNT2 += 1
+
+print(PTN)
+print("result1 =", CNT1)
+print("result2 =", CNT2)
