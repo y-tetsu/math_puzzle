20508d9c43279e80084c3f8dfa67b9d5e14b9a4b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-03-16 23:58:18 +0900

    mod if condition

--- q01.py
+++ q01.py
@@ -7,9 +7,12 @@ Q01
 N = 11
 
 while True:
-    if str(N) == str(N)[::-1] and \
-    "{0:o}".format(N) == "{0:o}".format(N)[::-1] and \
-    "{0:b}".format(N) == "{0:b}".format(N)[::-1]:
+    DEC_CHECK = str(N) == str(N)[::-1]
+    OCT_CHECK = "{0:o}".format(N) == "{0:o}".format(N)[::-1]
+    BIN_CHECK = "{0:b}".format(N) == "{0:b}".format(N)[::-1]
+
+    if DEC_CHECK and OCT_CHECK and BIN_CHECK:
         print(N)
         break
+
     N += 2
