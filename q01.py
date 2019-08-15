e968e6e46532c8ad75c9e109a0787d00b83fd4b1
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2019-08-15 16:04:58 +0900

    mod Q01

--- q01.py
+++ q01.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 """
 Q01
 """
 
 N = 11
+
 while True:
-    if f'{N}' == f'{N}'[::-1] and f'{N:o}' == f'{N:o}'[::-1] and f'{N:b}' == f'{N:b}'[::-1]:
+    dec_s, oct_s, bin_s = f'{N}', f'{N:o}', f'{N:b}'
+
+    if dec_s == dec_s[::-1] and oct_s == oct_s[::-1] and bin_s == bin_s[::-1]:
         print(N)
         break
+
     N += 2
