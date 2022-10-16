9eb64970c51ceaad1813be9a7a4ac78a08e96a70
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2022-10-16 15:52:23 +0900

    mod format

--- q12.py
+++ q12.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""Q12
 """
-Q12
-"""
-
 from decimal import Decimal
 import re
 
-i = 1
-while True:
-    SQRT = Decimal(i).sqrt()
-    if len(set(list(re.sub(r'^\d+\.', "", "{0:.10f}".format(SQRT))))) == 10:
-        print("少数部分のみの場合 :  " + str(i) + " の平方根は " + "{0:.10f}".format(SQRT))
-    if len(set(list("{0:.10f}".format(SQRT).replace(".", ""))[0:10])) == 10:
-        print("整数部分を含む場合 : " + str(i) + " の平方根は " + "{0:.10f}".format(SQRT))
-        break
-    i += 1
+
+if __name__ == '__main__':
+    i = 1
+    while True:
+        sqrt = Decimal(i).sqrt()
+        f = set(list(re.sub(r'^\d+\.', "", "{0:.10f}".format(sqrt))))
+        if len(f) == 10:
+            f_comment = "少数部分のみの場合 :  "
+            print(f_comment + str(i) + " の平方根は " + "{0:.10f}".format(sqrt))
+        a = set(list("{0:.10f}".format(sqrt).replace(".", ""))[0:10])
+        if len(a) == 10:
+            a_comment = "整数部分を含む場合 : "
+            print(a_comment + str(i) + " の平方根は " + "{0:.10f}".format(sqrt))
+            break
+        i += 1
