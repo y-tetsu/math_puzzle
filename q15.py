aa08569c1aa43154ff762f58f5c68ebbff591b8b
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2022-11-06 18:21:43 +0900

    update q15

--- q15.py
+++ q15.py
@@ -1,55 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Q15
+"""Q15
 """
 
 MAX_STEP = 10
 MAX_MOVE = 3 + 1
+A = 0
+B = 1
 
 
-def up_a(pos_a, pos_b, route_a, route_b):
-    """
-    Aさんが階段を上る
-    """
-    if pos_b - pos_a == 0:
+def updown(pos_a, pos_b, route_a, route_b, person):
+    """Aさんが階段を上り、Bさんが階段を下りる"""
+    if person == A and pos_b - pos_a == 0:
         print("> A =", route_a, " B =", route_b)
         return 1
-
     if pos_b - pos_a < 0:
         return 0
-
-    next_move = pos_b - pos_a
-    if pos_b - pos_a > MAX_MOVE:
-        next_move = MAX_MOVE
-
-    # Aを動かす
-    ret = 0
-    for i in range(1, next_move + 1):
-        next_a = pos_a + i
-        ret += down_b(next_a, pos_b, route_a + [next_a], route_b)
-
-    return ret
+    next_move = min(pos_b - pos_a, MAX_MOVE)
+    cnt = 0
+    for i in range(1, next_move+1):
+        if person == A:
+            next_pos = pos_a + i
+            cnt += updown(next_pos, pos_b, route_a + [next_pos], route_b, B)
+        else:
+            next_pos = pos_b - i
+            cnt += updown(pos_a, next_pos, route_a, route_b + [next_pos], A)
+    return cnt
 
 
-def down_b(pos_a, pos_b, route_a, route_b):
-    """
-    Bさんが階段を下りる
-    """
-    if pos_b - pos_a < 0:
-        return 0
-
-    next_move = pos_b - pos_a
-    if next_move > MAX_MOVE:
-        next_move = MAX_MOVE
-
-    # Bを動かす
-    ret = 0
-    for i in range(1, next_move + 1):
-        next_b = pos_b - i
-        ret += up_a(pos_a, next_b, route_a, route_b + [next_b])
-
-    return ret
-
-
-print("RESULT =", up_a(0, MAX_STEP, [0], [MAX_STEP]))
+print("cnt =", updown(0, MAX_STEP, [0], [MAX_STEP], A))
