f2f1fe5bbb0c028950f85950ea1d05a2e35a9dc8
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-06-20 18:45:44 +0900

    update q18

--- q18.py
+++ q18.py
@@ -1,32 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""Q18
 """
-Q18
-"""
-
 import math
 
-N = 2
 
-
-def check(num, strowberry, square):
+def check(num, ichigo, square):
+    """平方数のチェック
     """
-    チェック
-    """
-    if len(strowberry) == num:
-        if strowberry[-1] + strowberry[0] in square:
+    if len(ichigo) == num:
+        if ichigo[-1]+ichigo[0] in square:
             print("N =", num)
-            print(strowberry)
+            print(ichigo)
             return True
     else:
-        for i in list({j for j in range(1, num + 1)} - set(strowberry)):
-            if strowberry[-1] + i in square:
-                if check(num, strowberry + [i], square):
+        for i in list({j for j in range(1, num+1)} - set(ichigo)):
+            if ichigo[-1]+i in square:
+                if check(num, ichigo+[i], square):
                     return True
     return False
 
 
-while True:
-    if check(N, [1], [i**2 for i in range(2, int(math.sqrt(N * 2)))]):
-        break
-    N += 1
+if __name__ == '__main__':
+    n = 2  # 切り分ける個数
+    while True:
+        square = [i**2 for i in range(2, int(math.sqrt(n * 2)))]
+        if check(n, [1], square):
+            break
+        n += 1
