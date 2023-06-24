565cb64d0698eea5c0309848b05d7ab5f692ee02
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-06-24 15:01:21 +0900

    mod format

--- q36.py
+++ q36.py
@@ -1,21 +1,26 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Q36
+"""Q36
 """
 
-N = 1
 
-while N <= 50:
-    CNT = 1
+def solve():
+    """解答
+    """
+    n = 1
+    while n <= 50:
+        cnt = 1
+        while True:
+            num = 7 * int("{0:b}".format(cnt))
+            if num % n == 0:
+                if str(num) == str(num)[::-1]:
+                    if n != 13:
+                        if '0' in str(num):
+                            print(f"0 {n} * {int(num/n)} = {num}")
+                        else:
+                            print(f"  {n} * {int(num/n)} = {num}")
+                break
+            cnt += 1
+        n += 1
 
-    while True:
-        NUM = 7 * int("{0:b}".format(CNT))
 
-        if NUM % N == 0:
-            if str(NUM) == str(NUM)[::-1]:
-                print("N =", N, " CNT =", int(NUM / N), " NUM =", NUM)
-            break
-
-        CNT += 1
-    N += 1
+if __name__ == '__main__':
+    solve()
