85914384378a3dca713cd9300a7251494df89ef0
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-06-24 15:18:54 +0900

    mod format

--- q40.py
+++ q40.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Q40
 """
-Q40
-"""
-
 import itertools
 
-MAX_CNT = 0
-MAX_CARD = [i for i in range(1, 10)]
 
-for i in itertools.permutations([1, 2, 3, 4, 5, 6, 7, 8, 9], 9):
-    card = list(i)
-    pre_card = card[:]
-    cnt = 0
+def solve():
+    """解答
+    """
+    MAX_CNT = 0
+    MAX_CARD = [i for i in range(1, 10)]
+    for i in itertools.permutations([1, 2, 3, 4, 5, 6, 7, 8, 9], 9):
+        card = list(i)
+        pre_card = card[:]
+        cnt = 0
+        while True:
+            if card[0] == 1:
+                break
+            card[:card[0]] = card[card[0]-1::-1]
+            cnt += 1
+        if cnt > MAX_CNT:
+            MAX_CNT = cnt
+            MAX_CARD = pre_card[:]
+    print(', '.join([str(i) for i in MAX_CARD]))
 
-    while True:
-        if card[0] == 1:
-            break
-        card[:card[0]] = card[card[0]-1::-1]
-        cnt += 1
 
-    if cnt > MAX_CNT:
-        MAX_CNT = cnt
-        MAX_CARD = pre_card[:]
-
-print(MAX_CNT, MAX_CARD)
+if __name__ == '__main__':
+    solve()
