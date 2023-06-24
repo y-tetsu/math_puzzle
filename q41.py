d326050d836de0bb959d4b0a2cd3f4354b0bcd9d
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-06-24 15:23:07 +0900

    mod format

--- q41.py
+++ q41.py
@@ -1,20 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Q41
+"""Q41
 """
 
-CNT = 0
 
-for i in range(65536):
-    ip_bin = "{0:016b}".format(i) + "{0:016b}".format(i)[::-1]
-    ip_dec = str(int(ip_bin[0:8], 2))
-    ip_dec += str(int(ip_bin[8:16], 2))
-    ip_dec += str(int(ip_bin[16:24], 2))
-    ip_dec += str(int(ip_bin[24:32], 2))
+def solve():
+    """解答
+    """
+    cnt = 0
+    for i in range(65536):
+        ip_bin = "{0:016b}".format(i) + "{0:016b}".format(i)[::-1]
+        ip_dec = str(int(ip_bin[0:8], 2))
+        ip_dec += str(int(ip_bin[8:16], 2))
+        ip_dec += str(int(ip_bin[16:24], 2))
+        ip_dec += str(int(ip_bin[24:32], 2))
+        if len(set(ip_dec)) == 10:
+            print(ip_dec, ip_bin)
+            cnt += 1
+    print("cnt =", cnt)
 
-    if len(set(ip_dec)) == 10:
-        print(ip_dec, ip_bin)
-        CNT += 1
 
-print("result =", CNT)
+if __name__ == '__main__':
+    solve()
