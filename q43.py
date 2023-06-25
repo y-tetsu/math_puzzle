476be50785fb5fdd9250247a19dc13554ea1af5d
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-06-25 16:16:18 +0900

    fix format

--- q43.py
+++ q43.py
@@ -5,58 +5,53 @@ START = [i for i in range(1, 2*N+1)]
 GOAL = START[::-1]
 
 
-def shufful(card, ini):
+def sove():
+    """解答
     """
-    シャッフル
+    print('cnt =', search([START], [GOAL], 0, 0))
+
+
+def shufful(card, ini):
+    """シャッフル
     """
     tmp1 = card[:]
     tmp2 = card[ini:ini+N]
-
     del tmp1[ini:ini+N]
-
     return tmp1[:] + tmp2[:]
 
 
 def reverse_shufful(card, ini):
-    """
-    逆シャッフル
+    """逆シャッフル
     """
     tmp1 = card[0:ini]
     tmp2 = card[-N:]
     tmp3 = card[ini:-N]
-
     return tmp1[:] + tmp2[:] + tmp3[:]
 
 
 def search(start_cards, end_cards, start_depth, end_depth):
-    """
-    探索
+    """探索
     """
     # 開始側から探索
     next_start_cards = []
-
     for start in start_cards:
         for i in range(N):
             tmp = shufful(start, i)
             next_start_cards += [tmp]
-
     for start in next_start_cards:
         if start in end_cards:
             return start_depth + 1 + end_depth
-
     # 終了側から探索
     next_end_cards = []
-
     for end in end_cards:
         for i in range(N):
             tmp = reverse_shufful(end, i)
             next_end_cards += [tmp]
-
     for start in next_start_cards:
         if start in next_end_cards:
             return start_depth + 1 + end_depth + 1
-
     return search(next_start_cards, next_end_cards, start_depth + 1, end_depth + 1)
 
 
-print(search([START], [GOAL], 0, 0))
+if __name__ == '__main__':
+    sove()
