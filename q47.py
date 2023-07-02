32496af85fcc0cb9201c219a2c8b9935a0c712f2
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-07-02 18:39:14 +0900

    mod format

--- q47.py
+++ q47.py
@@ -2,21 +2,23 @@
 """
 import itertools
 
-N = 4
-MEMO = {}
+N, MEMO = 4, {}
 
-for pattern in itertools.product([1, 0], repeat=N*N):
-    count = []
 
-    for col in range(N):
-        count_row, count_col = 0, 0
+def solve():
+    """解答
+    """
+    for pattern in itertools.product([1, 0], repeat=N*N):
+        count = []
+        for col in range(N):
+            count_row, count_col = 0, 0
+            for row in range(N):
+                count_row += pattern[col+row*N]
+                count_col += pattern[col*N+row]
+            count += [count_row, count_col]
+        MEMO[tuple(count)] = 1 if tuple(count) not in MEMO else MEMO[tuple(count)] + 1
+    print(sum(filter(lambda x: x == 1, map(lambda x: MEMO[x], MEMO))))
 
-        for row in range(N):
-            count_row += pattern[col+row*N]
-            count_col += pattern[col*N+row]
 
-        count += [count_row, count_col]
-
-    MEMO[tuple(count)] = 1 if tuple(count) not in MEMO else MEMO[tuple(count)] + 1
-
-print(sum(filter(lambda x: x == 1, map(lambda x: MEMO[x], MEMO))))
+if __name__ == '__main__':
+    solve()
