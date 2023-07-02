1293419385aa5b355c272419abf91ddb831048dc
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-07-02 18:43:08 +0900

    mod format

--- q48.py
+++ q48.py
@@ -2,28 +2,30 @@
 """
 
 
-def gray_loop(value, base):
+def solve():
+    """解答
     """
-    グレイコードのループ
+    print("when 808080 =", gray_loop("808080", 16))
+    print("when abcdef =", gray_loop("abcdef", 16))
+
+
+def gray_loop(value, base):
+    """グレイコードのループ
     """
     g_next = value
     cnt = 1
-
     while True:
         g_rshift = "0" + g_next[0:-1]
         g_exor = ""
-
         for i, j in zip(g_next, g_rshift):
             g_exor += str("{0:x}".format((int(i, base) - int(j, base)) % base))
-
         if g_exor == value:
             break
         else:
             cnt += 1
             g_next = g_exor
-
     return cnt
 
 
-print("result of 808080 =", gray_loop("808080", 16))
-print("result of abcdef =", gray_loop("abcdef", 16))
+if __name__ == '__main__':
+    solve()
--- q49.py
+++ q49.py
@@ -6,55 +6,47 @@ G1 = int("".join([str("10") for i in range(N)]), 2)
 G2 = int("".join([str("01") for i in range(N)]), 2)
 
 
-def check_s(num, starts, goals, memo, depth):
+def solve():
+    """解答
     """
-    スタート状態を反転させる
+    print("cnt =", check_s(N, [S], [G1, G2], [{}, {}], 0))
+
+
+def check_s(num, starts, goals, memo, depth):
+    """スタート状態を反転させる
     """
     if list(filter(lambda x: x in goals, starts)):
         return depth
-
     next_tables = []
     mask = (1 << (num*2)) - 1
-
     for table in starts:
         swap_mask = 0b111
-
         for _ in range(num*2):
             next_table = table ^ swap_mask
-
             if next_table not in memo[0]:
                 memo[0][next_table] = True
                 next_tables += [next_table]
-
             swap_mask = ((swap_mask << 1) | (swap_mask << 1) >> (num*2)) & mask
-
     return check_g(num, next_tables, goals, memo, depth + 1)
 
 
 def check_g(num, starts, goals, memo, depth):
-    """
-    ゴール状態を反転させる
+    """ゴール状態を反転させる
     """
     if list(filter(lambda x: x in goals, starts)):
         return depth
-
     next_goals = []
     mask = (1 << (num*2)) - 1
-
     for goal in goals:
         swap_mask = 0b111
-
         for _ in range(num*2):
             next_goal = goal ^ swap_mask
-
             if next_goal not in memo[1]:
                 memo[1][next_goal] = True
                 next_goals += [next_goal]
-
             swap_mask = ((swap_mask << 1) | (swap_mask << 1) >> (num*2)) & mask
-
     return check_s(num, starts, next_goals, memo, depth + 1)
 
 
 if __name__ == '__main__':
-    print("result =", check_s(N, [S], [G1, G2], [{}, {}], 0))
+    solve()
