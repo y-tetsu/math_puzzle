b7751410a125f38ef342d8bb58bfcb77cd93477f
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-07-02 18:33:42 +0900

    mod format

--- q46.py
+++ q46.py
@@ -2,20 +2,25 @@
 """
 import itertools
 
-N = 7
-C = 0
+N, C = 7, 0
+
+
+def solve():
+    """解答
+    """
+    print("result =", C)
+
 
 for i in itertools.permutations([i + 1 for i in range(N)]):
     ptn = list(i)
     cnt = 0
-
     for j in range(len(ptn)):
         k = ptn.index(j + 1)
-
         if j != k:
             ptn[j], ptn[k] = ptn[k], ptn[j]
             cnt += 1
-
     C += cnt
 
-print("result =", C)
+
+if __name__ == '__main__':
+    solve()
