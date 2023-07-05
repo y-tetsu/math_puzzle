e2807583cbbfff05f4e54ddf7bee81459231ed1d
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-07-05 18:49:00 +0900

    mod format

--- q51.py
+++ q51.py
@@ -2,35 +2,27 @@
 """
 
 
-def perfect_shuffle(num):
+def solve():
+    """解答
     """
-    2*(num-1)回のシャッフルでもとにもどるかどうか返す
+    cnt = 0
+    for i in range(1, 101):
+        cnt += perfect_shuffle(i)
+    print('cnt =', cnt)
+
+
+def perfect_shuffle(num):
+    """2*(num-1)回のシャッフルでもとにもどるかどうか返す
     """
     cards = [i for i in range(1, 2 * num + 1)]
     goal = cards[:]
-
     for _ in range(2 * (num - 1)):
         tmp = []
-
         for i in range(num):
             tmp += [cards[i], cards[i + num]]
-
         cards = tmp[:]
-
     return 1 if cards == goal else 0
 
 
-def check():
-    """
-    1から100までチェックする
-    """
-    cnt = 0
-
-    for i in range(1, 101):
-        cnt += perfect_shuffle(i)
-
-    return cnt
-
-
 if __name__ == '__main__':
-    print("result =", check())
+    solve()
--- q52.py
+++ q52.py
@@ -5,46 +5,47 @@ import itertools
 N = 8
 
 
-class Hourglass:
+def solve():
+    """解答
     """
-    砂時計
+    cnt = 0
+    for ptn in itertools.permutations([i + 1 for i in range(N)]):
+        cnt += check(ptn)
+    print("cnt =", cnt)
+
+
+class Hourglass:
+    """砂時計
     """
     def __init__(self, minute):
         self.capacity = self.remain = minute
 
     def count_down(self):
-        """
-        1分経過 & 空になったら通知
+        """1分経過 & 空になったら通知
         """
         if self.remain > 0:
             self.remain -= 1
-
             if self.remain == 0:
                 return 1
-
         return 0
 
     def upset(self):
-        """
-        ひっくり返す
+        """ひっくり返す
         """
         self.remain = self.capacity - self.remain
 
 
 def check(pattern):
-    """
-    砂時計が同時に落ちるかチェック
+    """砂時計が同時に落ちるかチェック
     """
     hourglasses = [Hourglass(i) for i in pattern]
     memo = {}
     ret = 0
     loop = True
-
     while loop:
         # 砂時計を一周する
         for i in range(len(pattern)):
             tpl = tuple([i] + [j.remain for j in hourglasses])
-
             # 確認済みの状態か
             if tpl in memo:
                 loop = False
@@ -52,10 +53,8 @@ def check(pattern):
             else:
                 # 今回のパターンを記憶する
                 memo[tpl] = True
-
                 # 1分経過
                 cnt = sum([j.count_down() for j in hourglasses])
-
                 # すべての砂が同時に落ちた
                 if cnt == len(hourglasses):
                     ret = 1
@@ -65,14 +64,8 @@ def check(pattern):
                     # ひっくり返す
                     for offset in range(hourglasses[i].capacity):
                         hourglasses[(i + offset) % len(hourglasses)].upset()
-
     return ret
 
 
 if __name__ == '__main__':
-    CNT = 0
-
-    for ptn in itertools.permutations([i + 1 for i in range(N)]):
-        CNT += check(ptn)
-
-    print("result =", CNT)
+    solve()
