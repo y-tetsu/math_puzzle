ef5b2ddaf5f4e7ade8ee4808e3fb40a65a83c0e0
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-07-17 16:55:29 +0900

    mod format

--- q58.py
+++ q58.py
@@ -10,18 +10,18 @@ def solve():
     """
     global STATUS, STEP
     while not [s for s in STATUS if s[1] == N]:
-        NEXT_STATUS = []
+        NEXT = []
         for s in STATUS:
             for b in range(s[1] + 1):
                 for c in range(s[2] + 1):
                     if s[2] > 0:
                         if s[0] - b - c + 1 >= 0:
-                            NEXT_STATUS += [[s[0] - b - c + 1, s[1] + c, s[2] + b - 1, s[3] + 1]]
+                            NEXT += [[s[0] - b - c + 1, s[1] + c, s[2] + b - 1, s[3] + 1]]
                     if s[0] - b - c >= 0:
-                        NEXT_STATUS += [[s[0] - b - c, s[1] + c, s[2] + b, s[3]]]
+                        NEXT += [[s[0] - b - c, s[1] + c, s[2] + b, s[3]]]
                     if s[0] - b - c - 1 >= 0:
-                        NEXT_STATUS += [[s[0] - b - c - 1, s[1] + c + 1, s[2] + b, s[3] + 1]]
-        STATUS = [j for i, j in enumerate(NEXT_STATUS) if j not in STATUS and i == NEXT_STATUS.index(j)]
+                        NEXT += [[s[0] - b - c - 1, s[1] + c + 1, s[2] + b, s[3] + 1]]
+        STATUS = [j for i, j in enumerate(NEXT) if j not in STATUS and i == NEXT.index(j)]
         STEP += 1
     print(STEP, 'min')
     print('cnt =', min([i[3] for i in STATUS if i[1] == N]))
