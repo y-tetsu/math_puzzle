baf18a256a29d38cac792745598189a1f1eb3cec
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-07-31 18:44:55 +0900

    mod format

--- q62.py
+++ q62.py
@@ -2,34 +2,34 @@
 """
 M = 5
 N = 4
-CNT = 0
+
+
+def solve():
+    """解答
+    """
+    cnt = 0
+    for i in range(M):
+        for j in range(N):
+            cnt += search(i, j, [])
+    print("cnt =", cnt // 2)
 
 
 def search(x, y, path):
-    """
-    一筆書きの経路を探索
+    """一筆書きの経路を探索
     """
     next_path = path[:]
     next_path += [(x, y)]
-
     if len(next_path) == M * N:
         return 1
-
     count = 0
-
     for delta_x, delta_y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         next_x = x + delta_x
         next_y = y + delta_y
-
         if (next_x, next_y) not in next_path:
             if 0 <= next_x < M and 0 <= next_y < N:
                 count += search(next_x, next_y, next_path)
-
     return count
 
 
-for i in range(M):
-    for j in range(N):
-        CNT += search(i, j, [])
-
-print("result =", CNT // 2)
+if __name__ == '__main__':
+    solve()
