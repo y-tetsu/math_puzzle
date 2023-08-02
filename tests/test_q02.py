674d794fddf87a134768edb795f0e9182f143139
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-08-02 19:24:49 +0900

    add test

---
+++ tests/test_q02.py
@@ -0,0 +1,12 @@
+"""Q02
+"""
+import unittest
+from common import solve
+import q02
+
+
+class TestQ02(unittest.TestCase):
+    def test_solve(self):
+        elp, lines = solve(q02)
+        self.assertLessEqual(elp, 10)
+        self.assertEqual(lines, ['5931 (5 * 9 * 31 = 1395)'])
