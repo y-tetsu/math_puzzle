95b1bc09c72ecd7fa2a4b6bbe4c4dedc25cfa411
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2023-08-02 19:57:20 +0900

    mod test

--- tests/test_q01.py
+++ tests/test_q01.py
@@ -10,12 +10,3 @@ class TestQ01(unittest.TestCase):
         elp, lines = solve(q01)
         self.assertLessEqual(elp, 1)
         self.assertEqual(lines, ['585'])
-
-    def test_kaibun_3(self):
-        self.assertEqual(q01.kaibun(3), 3)
-
-    def test_kaibun_9(self):
-        self.assertEqual(q01.kaibun(9), 9)
-
-    def test_kaibun_11(self):
-        self.assertEqual(q01.kaibun(11), 585)
